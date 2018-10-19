bc7cb1f4b6ac8437032b9424340e4dd31ff2b831
Filipe Sabella <filipe.sabella@nubank.com.br>
2018-10-19 16:59:14 -0300

    Bootstrap typescript + electron + react + parcel

---
+++ main.js
@@ -0,0 +1,57 @@
+// Basic init
+const electron = require('electron')
+const {
+  app,
+  BrowserWindow
+} = electron
+const path = require('path');
+const url = require('url');
+
+// Let electron reloads by itself when webpack watches changes in ./app/
+if (process.env.ELECTRON_START_URL) {
+  require('electron-reload')(__dirname)
+}
+
+// To avoid being garbage collected
+let mainWindow
+
+app.on('ready', () => {
+
+  let mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, './build/index.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+
+  mainWindow.loadURL(startUrl)
+
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
+app.on('activate', function() {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+});
