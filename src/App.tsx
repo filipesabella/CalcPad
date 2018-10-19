547498048c8445e2401c32175191ac49bf29da5a
Filipe Sabella <filipesabella@gmail.com>
2018-10-19 17:49:55 -0300

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react';
 
+import './styles/app.less';
+
 export class App extends React.Component {
-  render() {
-    return <div>hellooo</div>;
+  public render(): React.ReactNode {
+    return <div className="app">
+      <textarea
+        autofocus="true"
+        onChange={e => this.onchange(e)}></textarea>
+      <div className="results"></div>
+    </div>;
+  }
+
+  private onchange(e: React.ChangeEvent<HTMLTextAreaElement>): void {
+    const value = e.target.value;
+    console.log(value);
   }
 }
