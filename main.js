be9c8f135887267e8853058b13767e3b2f451fc6
Filipe Sabella <filipe.sabella@gmail.com>
2018-11-02 21:27:25 -0300

    Clean up main.js

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 // Basic init
-const electron = require('electron')
+const electron = require('electron');
+
 const {
   app,
   BrowserWindow,
+  Menu,
 } = electron
+
 const path = require('path');
 const url = require('url');
 
 // Let electron reloads by itself when webpack watches changes in ./app/
 if (process.env.ELECTRON_START_URL) {
-  require('electron-reload')(__dirname)
+  require('electron-reload')(__dirname);
 }
 
 // To avoid being garbage collected
-let mainWindow
+let mainWindow;
 
 app.on('ready', () => {
-
-  let mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
   });
@@ -25,26 +27,25 @@ app.on('ready', () => {
   const startUrl = process.env.ELECTRON_START_URL || url.format({
     pathname: path.join(__dirname, './build/index.html'),
     protocol: 'file:',
-    slashes: true
+    slashes: true,
   });
 
   mainWindow.loadURL(startUrl)
 
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    mainWindow = null;
   })
 
 })
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 });
 
@@ -52,6 +53,6 @@ app.on('activate', function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
 });
