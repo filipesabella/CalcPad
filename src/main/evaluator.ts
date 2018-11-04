ccf2eab142eea5ccd0bdb3dd654fefc73f57bbe2
Filipe Sabella <filipesabella@gmail.com>
2018-11-04 01:17:17 -0200

    Refactor

--- src/main/evaluator.ts
+++ src/main/evaluator.ts
@@ -9,32 +9,35 @@ import { isComment, parse, isAssignment } from './parser';
  * and returns
  * [3, 20, 40]
  */
-export function textToResults(text: string, decimalPlaces: number): string[] {
+export function textToResults(
+  text: string,
+  decimalPlaces: number = 2): string[] {
+
   const lines = text.split('\n');
-
-  let assignments = '';
-  return lines.map((line, i) => {
-    try {
-      if (line === '' || isComment(line)) {
-        return '';
-      } else {
-        const transformedLine = parse(line);
-        const result = eval(assignments + transformedLine);
-
-        if (isAssignment(line)) {
-          assignments += transformedLine + '\n';
+  return lines
+    .map(line => [line, parse(line)])
+    .reduce<[string[], string]>(
+      ([results, assignments], [line, parsedLine]) => {
+        if (line.trim() === '' || isComment(line)) {
+          return [results.concat(''), assignments];
         }
 
-        const numberToDisplay = Math.round(result) !== result
-          ? result.toFixed(decimalPlaces)
-          : result;
+        try {
+          const result = eval(assignments + parsedLine);
 
-        return numberToDisplay;
-      }
-    } catch (e) {
-      // console.error(e);
-      // hehe :v
-      return '-';
-    }
-  });
+          if (isAssignment(line)) {
+            assignments += parsedLine + '\n';
+          }
+
+          const numberToDisplay = Math.round(result) !== result
+            ? result.toFixed(decimalPlaces)
+            : result;
+
+          return [results.concat(numberToDisplay), assignments];
+        } catch (e) {
+          // console.error(e);
+          // hehe :v
+          return [results.concat('-'), assignments];
+        }
+      }, [[], ''])[0];
 }
