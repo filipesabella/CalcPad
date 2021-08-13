bcd02dda91e16699c1d4e664796d534aae3f3de7
Filipe Sabella <me@filipesabella.com>
2021-08-13 15:08:47 -0300

    Remove autoupdater, too much hassle

--- main.js
+++ main.js
@@ -10,9 +10,6 @@ const {
 const path = require('path');
 const url = require('url');
 const windowStateKeeper = require('electron-window-state');
-const {
-  autoUpdater
-} = require("electron-updater");
 
 // Let electron reloads by itself when webpack watches changes in ./app/
 if (process.env.ELECTRON_START_URL) {
@@ -47,27 +44,6 @@ app.on('ready', () => {
   });
 
   mainWindow.loadURL(startUrl);
-
-  autoUpdater.checkForUpdates();
-  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-    const dialogOpts = {
-      type: 'info',
-      buttons: ['Restart', 'Later'],
-      title: 'Application Update',
-      message: process.platform === 'win32' ? releaseNotes : releaseName,
-      detail: 'A new version has been downloaded. ' +
-        'Restart the application to apply the updates.'
-    };
-
-    dialog.showMessageBox(dialogOpts, (response) => {
-      if (response === 0) autoUpdater.quitAndInstall();
-    });
-  });
-
-  autoUpdater.on('error', message => {
-    console.error('There was a problem updating the application');
-    console.error(message);
-  });
 
   mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
