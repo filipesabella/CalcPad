deb23f30e8b8df1f74a8191a54c290aca1ec0c34
Filipe Sabella <me@filipesabella.com>
2022-03-15 15:39:14 -0300

    Create codemirror completions

---
+++ src/main/components/Completions.ts
@@ -0,0 +1,22 @@
+import { CompletionContext } from '@codemirror/autocomplete';
+
+const allMathFunctions = Object
+  .getOwnPropertyNames(Math)
+  .map(n => ({ label: n, type: 'function' }));
+
+export function completions(context: CompletionContext) {
+  const word = context.matchBefore(/\w*/);
+  if (!word || (word.from === word.to && !context.explicit)) {
+    return null;
+  } else {
+    const vars = Array.from(
+      context.state.doc.toString().matchAll(/\n(\w*)\s+=.*/g),
+      (m: string[]) => m[1]
+    ).map(v => ({ label: v, type: 'variable' }));
+
+    return {
+      from: word.from,
+      options: allMathFunctions.concat(vars),
+    };
+  }
+}
