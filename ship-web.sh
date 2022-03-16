b12f9cba0777d50fed9b459df8c9409c8c0fff3f
Filipe Sabella <me@filipesabella.com>
2022-03-16 17:41:45 -0300

    Fix web build

--- ship-web.sh
+++ ship-web.sh
@@ -1,7 +1,7 @@
 git checkout build
 git merge master -m "Merge master"
 yarn build-web
-mv build/index-html.html build/index.html
+mv docs/index-html.html docs/index.html
 git add -A docs
 git commit -m "Release"
 echo "Now git push origin build"
