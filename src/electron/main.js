69839042e637656f2dc10033c591cbbe3d3a05b4
Filipe Sabella <me@filipesabella.com>
2022-03-16 16:56:48 -0300

    Last fix I swear

--- src/electron/main.js
+++ src/electron/main.js
@@ -46,7 +46,7 @@ app.on('ready', () => {
   }
 
   const startUrl = process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, '../../build/index.html'),
+    pathname: path.join(__dirname, '../../build/index-desktop.html'),
     protocol: 'file:',
     slashes: true,
   });
