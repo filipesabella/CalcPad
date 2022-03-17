80e69bcffa9f0435704623363901de2e2a996720
Filipe Sabella <me@filipesabella.com>
2022-03-16 22:52:31 -0300

    For some reason mobileweb didn't like the other version

--- src/main/components/desktop/index.tsx
+++ src/main/components/desktop/index.tsx
@@ -1,8 +1,8 @@
+import '@fontsource/jetbrains-mono';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { FileStore } from '../../lib/store';
 import { App } from './App';
-require('typeface-jetbrains-mono');
 
 const store = new FileStore();
 store.init().then(() => {
